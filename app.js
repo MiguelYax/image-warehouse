73706406f7a95894f640dbb39d699623230f5cf8
Miguel Yax <mig_dj@hotmail.com>
2019-03-09 22:12:47 -0600

    variables paras es6

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
  * date 03/09/2019
  * main
  */
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var index = require('./routes/index');
-var upload = require('./routes/upload');
-var app = express();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+const upload = require('./routes/upload');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +27,7 @@ app.use('/', upload);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
