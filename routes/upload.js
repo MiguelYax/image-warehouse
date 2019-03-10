a8a0eac18d704d53b76b331cf27eeb5739fd81a1
Miguel Yax <mig_dj@hotmail.com>
2019-03-09 21:56:28 -0600

    se agrega eslint para validacion de codigo y estilo

--- routes/upload.js
+++ routes/upload.js
@@ -3,68 +3,70 @@
  * date 03/09/2019
  * image loader
  */
-const express = require("express");
+const express = require('express');
+const config = require('config');
 const router = express.Router();
 
-const multer = require("multer");
-
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: function(req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  }
+    destination: config.get('storage'),
+    filename: function(req, file, cb) {
+        cb(
+            null,
+            file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+        );
+    }
 });
 
 // Init Upload
 const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: function(req, file, cb) {
-    checkFileType(file, cb);
-  }
-}).single("myImage");
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: function(req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('myImage');
 
 // Check File Type
 function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+    );
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images Only!");
-  }
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
 }
 
-router.post("/upload", (req, res) => {
-  upload(req, res, err => {
-    if (err) {
-      res.render("index", {
-        msg: err
-      });
-    } else {
-      if (req.file == undefined) {
-        res.render("index", {
-          msg: "Error: No File Selected!"
-        });
-      } else {
-        res.render("index", {
-          msg: "File Uploaded!",
-          file: `uploads/${req.file.filename}`
-        });
-      }
-    }
-  });
+router.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            res.render('index', {
+                msg: err
+            });
+        } else {
+            if (req.file == undefined) {
+                res.render('index', {
+                    msg: 'Error: No File Selected!'
+                });
+            } else {
+                res.render('index', {
+                    msg: 'File Uploaded!',
+                    file: `uploads/${req.file.filename}`
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
