6914b0912f03541ba8d7dbb7d870a830c85947a9
Miguel Yax <mig_dj@hotmail.com>
2019-03-22 19:12:44 -0600

    add index.html route

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
+    $('#inputGroupFile02').on('change', function() {
+        //get the file name
+        var fileName = $(this).val();
+        //replace the "Choose a file" label
+        $(this)
+            .next('.custom-file-label')
+            .html(fileName);
+    });
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', {
+        list: [
+            {
+                date: '030319',
+                name: 'm.JPG'
+            },
+            {
+                date: '030319',
+                name: 'm.JPG'
+            },
+            {
+                date: '030319',
+                name: 'm.JPG'
+            },
+            {
+                date: '030319',
+                name: 'm.JPG'
+            },
+            {
+                date: '030319',
+                name: 'm.JPG'
+            },
+            {
+                date: '030319',
+                name: 'm.JPG'
+            },
+            {
+                date: '030319',
+                name: 'm.JPG'
+            }
+        ]
+    });
 });
 
+router.get('/index.html', function(req, res, next) {
+    res.render('index', {
+        list: []
+    });
+});
 module.exports = router;
