98bf918a18401821c0718b605c697f3699971da9
Miguel Yax <mig_dj@hotmail.com>
2019-04-04 23:11:55 -0600

    encripar contraseña de usuario

--- application/modules/user.js
+++ application/modules/user.js
@@ -1,19 +1,22 @@
 const query = require('./query');
 const uuidv4 = require('uuid/v4');
+const sha1 = require('sha1');
 
 let user = {
     singup: function(firstName, lastName, email, password, cb) {
         /**
          * @todo password se debe cifrar la contraseña :)
          */
+        let hash = sha1(password);
         let sql = `INSERT INTO IW_USER (UUID, FIRSTNAME, LASTNAME, EMAIL, \`PASSWORD\`) VALUES ('${uuidv4().replace(
             /-/g,
             '\\-'
-        )}', '${firstName}', '${lastName}', '${email}', '${password}');`;
+        )}', '${firstName}', '${lastName}', '${email}', '${hash}');`;
         query(sql, cb);
     },
     singin: function(email, password, cb) {
-        let sql = `SELECT * FROM IW_USER WHERE EMAIL = '${email}' AND PASSWORD = '${password}' `;
+        let hash = sha1(password);
+        let sql = `SELECT * FROM IW_USER WHERE EMAIL = '${email}' AND PASSWORD = '${hash}' `;
         query(sql, cb);
     },
     signout: function(email, cb) {
