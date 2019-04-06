498bbbcf86bd8c5d1bbbf657da1a1418dfa31f8e
Miguel Yax <mig_dj@hotmail.com>
2019-04-06 06:24:48 -0600

    lectura de imagen almacenada en el bucket s3

--- application/modules/upload.js
+++ application/modules/upload.js
@@ -11,11 +11,7 @@ const image = require('./image');
 const path = require('path');
 const config = require('config');
 
-aws.config.update({
-    secretAccessKey: config.mainBucket.secretAccessKey,
-    accessKeyId: config.mainBucket.accessKeyId,
-    region: config.mainBucket.region
-});
+aws.config.update(config.mainBucket);
 
 const s3 = new aws.S3();
 
@@ -32,8 +28,11 @@ const upload = multer({
     storage: multerS3({
         s3,
         bucket: config.mainBucket.name,
+        metadata: function(req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
         key: function(req, file, cb) {
-            file.uuid = image.uuid();
+            file.uuid = Date.now().toString();
             image.upload(
                 file.uuid,
                 file.originalname,
@@ -42,7 +41,6 @@ const upload = multer({
                 path.extname(file.originalname),
                 function(err, data) {
                     cb(null, file.uuid + path.extname(file.originalname));
-                    // cb(null, file.uuid);
                 }
             );
         }
@@ -50,55 +48,3 @@ const upload = multer({
 });
 
 module.exports = upload;
-// const config = require('config');
-// const image = require('./image');
-// const multer = require('multer');
-// const path = require('path');
-
-// // Set The Storage Engine
-// const storage = multer.diskStorage({
-//     destination: config.storage,
-//     filename: function(req, file, cb) {
-//         file.uuid = image.uuid();
-//         image.upload(
-//             file.uuid,
-//             file.originalname,
-//             0,
-//             req.session.uuid,
-//             path.extname(file.originalname),
-//             function(err, data) {
-//                 // console.log('insert');
-//                 cb(null, file.uuid + path.extname(file.originalname));
-//             }
-//         );
-//     }
-// });
-
-// // Init Upload
-// const upload = multer({
-//     storage: storage,
-//     limits: { fileSize: 1000000 },
-//     fileFilter: function(req, file, cb) {
-//         checkFileType(file, cb, req.session.uuid);
-//     }
-// }).single('myImage');
-
-// // Check File Type
-// function checkFileType(file, cb, uuidUser) {
-//     // Allowed ext
-//     const filetypes = /jpeg|jpg|png|gif/;
-//     // Check ext
-//     const extname = filetypes.test(
-//         path.extname(file.originalname).toLowerCase()
-//     );
-//     // Check mime
-//     const mimetype = filetypes.test(file.mimetype);
-
-//     if (mimetype && extname) {
-//         return cb(null, true);
-//     } else {
-//         cb('Error: Images Only!');
-//     }
-// }
-
-// module.exports = upload;
